09d39e09e21100f4c85d09576aa6610a5ea2f547
SatyaSingh-x11 <satyanarayansing41@gmail.com>
2026-03-20 18:55:23 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open("nearkart-cache").then(cache => {
+      return cache.addAll([
+        "/",
+        "/index.html",
+        "/assets/css/style.css"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
